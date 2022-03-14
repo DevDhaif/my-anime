6790063a986d461e65ed9909aaf217c6f4de301c
DevDhaif <devdhaif@gmail.com>
2022-03-14 23:57:44 +0200

    upcoming page

--- src/pages/Item.jsx
+++ src/pages/Item.jsx
@@ -46,62 +46,62 @@ function Item() {
 
   return (
        
-            <main className='min-h-screen   py-4 bg-center bg-no-repeat bg-cover bg-opacity-10 ' style={{ 
-                    backgroundImage: `url(${animeDetails.image_url})`}}>
-                
-                
-                    <div class="p-4 space-y-8   bg-bg-dark/70  md:h-full text-white   justify-around    rounded-md shadow-md shadow-white ">
-                    <section class="w-full md:flex md:flex-row   justify-start space-x-4">
-                    <img class="w-full  object-cover max-h-96  " src={animeDetails.image_url} alt=""/>
-                    <div class="space-y-3 px-2 py-8   h-full  w-full">
-                        <p>{animeDetails.title}</p>
-                        <p>{animeDetails.airing}   </p>
-                        <p class="max-w-sm">{animeDetails.synopsis}</p>
-                        <p>{animeDetails.type}</p>
-                        <p>{animeDetails.episodes}</p>
-                        <p>{animeDetails.score}</p>
-                        <p>{animeDetails.rated}</p>
-                        <p>{animeDetails.members}</p>
-                        
-                    </div>
-
-
-
-                    </section>
-                    <section className='bg-bg-dark/50 w-full text-white px-4 py-8 space-y-4'>
-                    
-                    <h1>Episodes</h1>
-                    <Swiper
-                    modules={[Navigation, Pagination, Scrollbar, A11y,Autoplay]}
-                                    spaceBetween={50}
-                                    slidesPerView={2}
-                                    loop={true}
-                                    autoplay={{
-                                        delay: 1000,
-                                        disableOnInteraction: false
-                                    }}
-                                    scrollbar={{ draggable: true, dragSize: 24 }}
-                                    >
-                                    {anime.map((ep)=>(
-                                    <SwiperSlide key={ep.episode_id} className="bg-bg-prime ">
-                                    <div className='inline-flex items-center h-24 space-x-2 p-6 justify-center'>
-                                            <p className=''>{ep.episode_id} {ep.title}</p>
-                                            <a className=' text-lg' href={ep.video_url} target="_blank" rel="noopener noreferrer">
-                                            <GoEye className='fill-blue-200 hover:fill-blue-700'/>
-                                            </a>
-                                    </div>
-                                    </SwiperSlide>
-                                    ))}
-                                    
-                                    </Swiper>
-    
-                    </section>
-                
-                
-                </div>
-                
-            </main>
+        <main className='min-h-screen   py-4 bg-center bg-no-repeat bg-cover bg-opacity-10 ' style={{ 
+                backgroundImage: `url(${animeDetails.image_url})`}}>
             
+            
+                <div class="p-4 space-y-8   bg-bg-dark/70  md:h-full text-white   justify-around    rounded-md shadow-md shadow-white ">
+                <section class="w-full md:flex md:flex-row   justify-start space-x-4">
+                  <img class="w-full  object-cover max-h-96  " src={animeDetails.image_url} alt=""/>
+                  <div class="space-y-3 px-2 py-8   h-full  w-full">
+                      <p>{animeDetails.title}</p>
+                      <p>{animeDetails.airing}   </p>
+                      <p class="max-w-sm">{animeDetails.synopsis}</p>
+                      <p>{animeDetails.type}</p>
+                      <p>{animeDetails.episodes}</p>
+                      <p>{animeDetails.score}</p>
+                      <p>{animeDetails.rated}</p>
+                      <p>{animeDetails.members}</p>
+                      
+                  </div>
+
+
+
+                </section>
+                <section className='bg-bg-dark/50 w-full text-white px-4 py-8 space-y-4'>
+                
+                  <h1>Episodes</h1>
+                <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y,Autoplay]}
+                                spaceBetween={50}
+                                slidesPerView={2}
+                                loop={true}
+                                autoplay={{
+                                    delay: 1000,
+                                    disableOnInteraction: false
+                                }}
+                                scrollbar={{ draggable: true, dragSize: 24 }}
+                                >
+                                {anime.map((ep)=>(
+                                <SwiperSlide key={ep.episode_id} className="bg-bg-prime ">
+                                <div className='inline-flex items-center h-24 space-x-2 p-6 justify-center'>
+                                        <p className=''>{ep.episode_id} {ep.title}</p>
+                                        <a className=' text-lg' href={ep.video_url} target="_blank" rel="noopener noreferrer">
+                                        <GoEye className='fill-blue-200 hover:fill-blue-700'/>
+                                        </a>
+                                </div>
+                                </SwiperSlide>
+                                ))}
+                                
+                                </Swiper>
+  
+                </section>
+               
+              
+              </div>
+              
+        </main>
+        
                     
                         
             
